7732fbd360f1481d62e1e36cae90f60269ccb352
robinr.rao@gmail.com <robinr.rao@gmail.com@2b3fbb68-500b-832b-272e-79c097831314>
2014-03-04 21:14:00 +0000

    Java Thread Projects.

---
+++ trunk/threads/ThreadClassDemo/src/DisplayMessage.java
@@ -0,0 +1,17 @@
+
+public class DisplayMessage implements Runnable {
+	
+	private String message;
+	public DisplayMessage(String message)
+	{
+		this.message = message;
+	}
+
+	@Override
+	public void run() {
+		while(true)
+		{
+			System.out.println(message);
+		}
+	}
+}
